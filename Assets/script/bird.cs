44023191b2c041922e2b72b2d71db20887ae9fc6
zxzhu <354328995@qq.com>
2017-06-30 23:43:15 +0800

    first commit

---
+++ Assets/script/bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+	private Rigidbody2D flappy;
+	private PlayMakerFSM fsm;
+
+	void Awake(){
+//		Vector2 velocity = flappy.velocity;
+//		velocity.y = 0.5;
+//		flappy.velocity.y = velocity;
+		flappy = GetComponent<Rigidbody2D> ();
+		fsm = GetComponent<PlayMakerFSM> ();
+	}
+
+	public void addForce(Vector2 force){
+		Vector2 velocity = flappy.velocity;
+		velocity.y = 0;
+		if (flappy.velocity.y < 0) {
+			flappy.velocity = velocity;
+		}
+		flappy.AddForce (force);
+	}
+
+	public void OnCollisionEnter2D(Collision2D col){
+		fsm.SendEvent ("GameOver");
+	}
+}
